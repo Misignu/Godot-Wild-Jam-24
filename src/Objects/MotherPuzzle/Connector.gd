4242947352576c7b0403d71362c904d2ea8c50c6
Leµg <58122451+Leuug@users.noreply.github.com>
2020-08-16 18:58:16 -0300

    Implementing Mother Puzzle Energy Transmitters #1

--- src/Objects/MotherPuzzle/Connector.gd
+++ src/Objects/MotherPuzzle/Connector.gd
@@ -1,4 +1,22 @@
 extends Node2D
 
+var animate: bool
+var tween_state: bool
+
+onready var tween: Tween = $Tween
+onready var timer: Timer = $Timer
+onready var shape: Polygon2D = $Shape
+
+
+# animate the rotation of the connector
 func _on_Button_pressed() -> void:
-	rotate(deg2rad(90))
+	shape.call_discharge()
+	
+	if timer.is_stopped():
+		timer.wait_time = 0.5
+		animate = tween.interpolate_property(self, "rotation", rotation, rotation + PI/2, 0.5)
+		
+		if not tween.is_active():
+			tween_state = tween.start()
+		
+		timer.start()
